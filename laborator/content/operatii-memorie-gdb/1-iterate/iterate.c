372d3505e06cb90e459a129c0c367d22291634b2
Malanca Daniel <malanca.daniel23@gmail.com>
2024-03-14 10:07:10 +0200

    solutie exercitiu 1

--- laborator/content/operatii-memorie-gdb/1-iterate/iterate.c
+++ laborator/content/operatii-memorie-gdb/1-iterate/iterate.c
@@ -9,11 +9,23 @@
  * din 2 in 2 octeti si apoi din 4 in 4.
  */
 
-int main(void)
-{
-	int v[] = {0xCAFEBABE, 0xDEADBEEF, 0x0B00B135, 0xBAADF00D, 0xDEADC0DE};
+int main(void) {
+    int v[] = {0xCAFEBABE, 0xDEADBEEF, 0x0B00B135, 0xBAADF00D, 0xDEADC0DE};
 
-	(void) v;
+    for (int i = 0; i < 5; i++) printf("%p -> 0x%x\n", &v[i], v[i]);
+    printf("\n");
 
-	return 0;
+    for (int i = 0; i < 9; i++)
+        printf(
+            "%p -> 0x%04hx\n",
+            (char *)v + i * 2,
+            *(short *)((char *)v + i * 2));
+    printf("\n");
+
+    for (int i = 0; i < 17; i++)
+        printf("%p -> 0x%02hhx\n", (char *)v + i, *(char *)((char *)v + i));
+
+    (void)v;
+
+    return 0;
 }
