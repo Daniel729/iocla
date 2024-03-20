997ce21677f0c967d6481cf46172a3fb87f4bb38
Malanca Daniel <malanca.daniel23@gmail.com>
2024-03-20 19:08:07 +0200

    solutie ex 7

--- laborator/content/etapele-compilarii/07-implicit/main.c
+++ laborator/content/etapele-compilarii/07-implicit/main.c
@@ -2,13 +2,15 @@
 
 #include <stdio.h>
 
+extern long long compute_double(long long);
+
 int main() {
-	unsigned long long val = 0xFFFFFFFF;
-	unsigned long long double_val;
+    unsigned long long val = 0xFFFFFFFF;
+    unsigned long long double_val;
 
-	double_val = compute_double(val);
+    double_val = compute_double(val);
 
-	printf("2 * %llx = %llx\n", val, double_val);
+    printf("2 * %llx = %llx\n", val, double_val);
 
-	return 0;
+    return 0;
 }
