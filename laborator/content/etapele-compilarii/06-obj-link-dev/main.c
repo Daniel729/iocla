db9a00c6bd0a62ddbad6f55adaa48e4276bb1911
Malanca Daniel <malanca.daniel23@gmail.com>
2024-03-20 19:06:10 +0200

    solutie ex 6

--- laborator/content/etapele-compilarii/06-obj-link-dev/main.c
+++ laborator/content/etapele-compilarii/06-obj-link-dev/main.c
@@ -1,12 +1,20 @@
-int main(void)
-{
-	/*
-	 * TODO: Make it so you print:
-	 *    price is 21
-	 *    quantity is 42
-	 * without directly calling a printing function from an existing
-	 * library(do NOT use printf, fprintf, fwrite, ...).
-	 */
+extern int qty;
+extern void set_price(int);
+extern void print_price();
+extern void print_quantity();
 
-	return 0;
+int main(void) {
+    /*
+     * TODO: Make it so you print:
+     *    price is 21
+     *    quantity is 42
+     * without directly calling a printing function from an existing
+     * library(do NOT use printf, fprintf, fwrite, ...).
+     */
+
+    set_price(21);
+    print_price();
+    qty = 42;
+    print_quantity();
+    return 0;
 }
