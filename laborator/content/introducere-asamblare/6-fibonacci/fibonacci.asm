bd783d63af595c3e5ab99c3c44113c1a68b41375
Malanca Daniel <malanca.daniel23@gmail.com>
2024-04-03 19:36:09 +0300

    solutii ex 6

--- laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
+++ laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
@@ -6,7 +6,15 @@ section .text
 
 main:
     mov eax, 7       ; vrem sa aflam al N-lea numar; N = 7
+    mov ecx, eax
+    xor eax, eax
+    mov ebx, 1
+calc_loop:
+    lea edx, [eax + ebx]
+    mov eax, ebx
+    mov ebx, edx     
+    loop calc_loop
 
     ; TODO: calculati al N-lea numar fibonacci (f(0) = 0, f(1) = 1)
-
+    PRINTF32 `%d\n\x0`, eax ;
     ret
