041eb97f1ad58cc1dc43221b14dc68983070e745
Malanca Daniel <malanca.daniel23@gmail.com>
2024-04-03 19:36:03 +0300

    solutii ex 5

--- laborator/content/introducere-asamblare/5-min/min.asm
+++ laborator/content/introducere-asamblare/5-min/min.asm
@@ -8,6 +8,10 @@ main:
     ;cele doua numere se gasesc in eax si ebx
     mov eax, 4
     mov ebx, 1
+    cmp eax, ebx
+    jb do_nothing
+    xchg eax, ebx
+do_nothing:
     ; TODO: aflati minimul
     PRINTF32 `%d\n\x0`, eax ; afiseaza minimul
 
