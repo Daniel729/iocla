c07022f8b62551b70b7d97e6aa0afa5d48523797
Malanca Daniel <malanca.daniel23@gmail.com>
2024-05-15 18:49:53 +0300

    solutie ex 2

--- laborator/content/interactiune-c-assembly/01-2-max-c-calls/max.asm
+++ laborator/content/interactiune-c-assembly/01-2-max-c-calls/max.asm
@@ -4,24 +4,28 @@ section .text
 
 global get_max
 
+;; unsigned int get_max(unsigned int* arr, unsigned int len, unsigned int* pos);
 get_max:
 	push ebp
 	mov ebp, esp
 
 	; save ebx in callee
 	push ebx
-
+	push esi
 	; [ebp+8] is array pointer
 	; [ebp+12] is array length
 
 	mov ebx, [ebp+8]
 	mov ecx, [ebp+12]
+	mov esi, [ebp+16]
 	xor eax, eax
 
 compare:
 	cmp eax, [ebx+ecx*4-4]
 	jge check_end
 	mov eax, [ebx+ecx*4-4]
+	mov [esi], ecx
+	dec dword [esi]
 check_end:
 	loopnz compare
 
